f1790683b81bc92bbf408e76a2b877f50ea6ec40
knausb <briank.lists@gmail.com>
2015-05-08 15:09:57 -0700

    created baf_plot and bedify

---
+++ R/baf_plot.R
@@ -0,0 +1,20 @@
+
+
+#' @title baf_plot
+#' @rdname baf_plot
+#' 
+#' @description BAlleleFreq plot.
+#' 
+#' 
+#' 
+#' 
+#' @references Laurie, Cathy C, Kimberly F Doheny, Daniel B Mirel, Elizabeth W Pugh, Laura J Bierut, Tushar Bhangale, Frederick Boehm, Neil E Caporaso, Marilyn C Cornelis, Howard J Edenberg and others.
+#'   2010. Quality control and quality assurance in genotypic data for genome-wide association studies.
+#'   Genetic Epidemiology 34(6): 591--602.
+#' 
+baf_plot <- function(){
+  
+  
+}
+
+
