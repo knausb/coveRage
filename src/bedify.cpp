34aade8f80077e33e4c446c8316d5a10026c23fa
knausb <briank.lists@gmail.com>
2015-05-18 12:59:22 -0700

    added os to .travis.yml

--- src/bedify.cpp
+++ src/bedify.cpp
@@ -33,7 +33,7 @@ Rcpp::List bedify( Rcpp::StringMatrix myBed, Rcpp::IntegerMatrix myData ) {
   
   // Scroll over bed rows (features).
   for(int i=0; i<myBed.nrow(); i++){
-    Rcpp::Rcout << "Feature: " << i << "\n";
+//    Rcpp::Rcout << "Feature: " << i << "\n";
     
     // Count rows
     std::string temp = Rcpp::as< std::string >(myBed(i,1));
@@ -59,19 +59,18 @@ Rcpp::List bedify( Rcpp::StringMatrix myBed, Rcpp::IntegerMatrix myData ) {
     int j=0;  // matrix to bedify row counter
     int k = 0; // Out matrix row counter.
     while(myData(j,0) < start){ j++;}
-    Rcpp::Rcout << "  Found feature.\n";
-    Rcpp::Rcout << "  myData(j, 0): " << myData(j, 0) << ".\n";
-    Rcpp::Rcout << "  start: " << start << ".\n";
-    Rcpp::Rcout << "  end: " << end << ".\n";
+//    Rcpp::Rcout << "  Found feature.\n";
+//    Rcpp::Rcout << "  myData(j, 0): " << myData(j, 0) << ".\n";
+//    Rcpp::Rcout << "  start: " << start << ".\n";
+//    Rcpp::Rcout << "  end: " << end << ".\n";
 
     // Process feature.
     while(myData(j,0) <= end + 0){
 
-      Rcpp::Rcout << "j is: " << j;
-      Rcpp::Rcout << ", k is: " << k;
-      Rcpp::Rcout << ", start is " << start;
-//      Rcpp::Rcout << ", the difference is: " << j - start;
-      Rcpp::Rcout << "\n";
+//      Rcpp::Rcout << "j is: " << j;
+//      Rcpp::Rcout << ", k is: " << k;
+//      Rcpp::Rcout << ", start is " << start;
+//      Rcpp::Rcout << "\n";
       
       // Handle when missing data at begining.
       while(myData(j,0) > start + k){
