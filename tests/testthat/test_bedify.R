3113bfde087f9513e838f39be44a76a5c523a802
knausb <briank.lists@gmail.com>
2015-06-17 16:34:13 -0700

    updated test_bedify.R

--- tests/testthat/test_bedify.R
+++ tests/testthat/test_bedify.R
@@ -6,22 +6,50 @@ ex_file <- system.file("extdata", "sc10_4k.mpileup.gz", package = "covR")
 stats <- file_stats(ex_file, verbose=0)
 x1 <- read_matrix(ex_file, nrows=stats[2], cols=1:stats[3], verbose=0)
 
-
+# Create bed data
 myBed <- matrix(ncol=4, nrow=5)
-myBed[,1] <- "sc12"
+#myBed[,1] <- "sc12"
 myBed[,1] <- "Supercontig_1.10"
-
 myBed[,2] <- c(1, 1001, 1011, 9100, 10600)
 myBed[,3] <- c(10, 1020, 1030, 9110, 10607)
 myBed[,4] <- paste("gene", 1:5, sep="_")
 
 
+# Without fill_missing.
 myGenes <- bedify(myBed, x1)
-myGenes <- bedify(myBed, x1, fill_missing=1)
-
 
 test_that("myGenes contains the correct number of genes", {
   expect_equal(length(myGenes), 5)
-  
+})
+
+test_that("myGenes contains the correct number of rows", {
+  expect_equal(nrow(myGenes[[1]]), 10)
+  expect_equal(nrow(myGenes[[2]]), 20)
+  expect_equal(nrow(myGenes[[3]]), 20)
+  expect_equal(nrow(myGenes[[4]]), 5)
+  expect_equal(nrow(myGenes[[5]]), 8)
+  lapply(myGenes, nrow)
+})
+
+
+
+# Without fill_missing.
+myGenes <- bedify(myBed, x1, fill_missing=1)
+
+test_that("myGenes contains the correct number of genes", {
+  expect_equal(length(myGenes), 5)
+})
+
+test_that("myGenes contains the correct number of rows", {
+  expect_equal(nrow(myGenes[[1]]), 10)
+  expect_equal(nrow(myGenes[[2]]), 20)
+  expect_equal(nrow(myGenes[[3]]), 20)
+  expect_equal(nrow(myGenes[[4]]), 11)
+  expect_equal(nrow(myGenes[[5]]), 8)
+#  lapply(myGenes, nrow)
+})
+
+test_that("myGenes contains NAs", {
+  expect_equal(sum(is.na(myGenes[[4]][1:6,3:54])), 312)
 })
 
